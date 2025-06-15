7d1ea076b5705cca273a847bd9fad330ae3d0c19
jun7861-hash <jun.estropigan@gmail.com>
2025-06-15 16:36:17 +0800

    update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // <=== enables static exports
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
