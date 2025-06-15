4c3dcdefa8ae53f1c1af93493fc7e7b29df4e844
jun7861-hash <jun.estropigan@gmail.com>
2025-06-15 17:00:36 +0800

    update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_ACTIONS || false;
+const repoName = 'my-lair';
+
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+
+  ...(isGithubPages && {
+    basePath: `/${repoName}`,
+    assetPrefix: `/${repoName}/`,
+  }),
 };
 
 module.exports = nextConfig;
