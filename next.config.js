a1ae84f1b8594f20ca32ef271ab7ee2a989dbe2e
jun7861-hash <jun.estropigan@gmail.com>
2025-06-25 22:00:04 +0800

    add environment variables for EmailJS integration

--- next.config.js
+++ next.config.js
@@ -10,4 +10,10 @@ module.exports = {
   images: {
     unoptimized: true,
   },
+  env: {
+    publicNewsKey: '5a10f779c5594b3e8b65b893d17c201f',
+    emailjsServiceId: '5a10f779c5594b3e8b65b893d17c201f',
+    emailjsTemplateId: 'template_sv4ts4b',
+    emailjsPublicKey: '5a10f779c5594b3e8b65b893d17c201f',
+  },
 };
